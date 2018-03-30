9dd6e3687489452bbbf58ee4cab0aa9886c70458
TheOriginalRobbiP <Robin@lastpiece.co.za>
2018-03-30 12:35:13 +0200

    Lambda function update

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,6 +4,9 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:eu-west-2:976290992126:deployPortfolioTopic')
+    
     s3 = boto3.resource('s3')
     
     portfolio_bucket = s3.Bucket('portfolio.robinpretorius.co.za')
@@ -18,5 +21,7 @@ def lambda_handler(event, context):
             portfolio_bucket.upload_fileobj(obj, nm,
             ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    print "Jobs done!"
+    topic.publish(Subject="Portfolio Deployed", Message = "Your portfolio has been deployed.")
     
     return 'Hello from Lambda'
