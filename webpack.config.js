e8045728d3d172a753a532ad24da0b0f00c8bf03
TheOriginalRobbiP <Robin@lastpiece.co.za>
2018-04-04 19:43:09 +0200

    js please

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,13 @@ module.exports = {
     module: {
         rules:[{
             test: /\.js$/,
-            exclude: /node_modules/
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options:{
+                    presets: ['latest']
+                }
+            }
         }]
     }
 }
